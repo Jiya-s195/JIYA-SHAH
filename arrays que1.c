1cc8488cd2154c3a267b209bf9e068b4c4c7fffe
Jiya-s195 <073ahome@gmail.com>
2025-09-09 16:57:27 +0530

    Add files via upload

---
+++ arrays que1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int values[5];
+printf("Enter 5 integers:\n");
+for(int i=0;i<5;i++){
+scanf("%d",&values[i]);
+}
+printf("The entered valueS are:\n");
+for(int i=0;i<5;i++){
+printf("%d\n",values[i]);
+
+}
+
+return 0;
+}
