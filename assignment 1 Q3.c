f63e71f183c6e2aa52e57ee2b12306e8697e55d8
Jiya-s195 <073ahome@gmail.com>
2025-10-04 23:42:56 +0530

    Add files via upload

---
+++ assignment 1 Q3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,multiplication;
+    printf("Enter the first number:");
+scanf("%d\n",&a);
+printf("Enter the second number:");
+scanf("%d\n",&b);
+multiplication = a*b;
+printf("The multiplication is : %d\n",multiplication);
+return 0;
+
+}
