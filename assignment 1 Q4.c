d44997048dd9ffa88af09526b3af837fa47065db
Jiya-s195 <073ahome@gmail.com>
2025-10-04 23:47:02 +0530

    Add files via upload

---
+++ assignment 1 Q4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,division;
+    printf("Enter the first number:");
+scanf("%d\n",&a);
+printf("Enter the second number:");
+scanf("%d\n",&b);
+division = a/b;
+printf("The division is : %d\n",division);
+return 0;
+
+}
