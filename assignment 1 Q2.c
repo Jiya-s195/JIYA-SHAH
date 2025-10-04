ad18514e4665c75b48bdcff24a5a271391194aaf
Jiya-s195 <073ahome@gmail.com>
2025-10-04 23:38:56 +0530

    Add files via upload

---
+++ assignment 1 Q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,difference;
+    printf("Enter the first number:");
+scanf("%d\n",&a);
+printf("Enter the second number:");
+scanf("%d\n",&b);
+difference = a-b;
+printf("The difference is : %d\n",difference);
+return 0;
+
+}
