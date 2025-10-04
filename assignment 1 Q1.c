8be1ac129fb5a7660fa39ab4e68882f2007f7cf3
Jiya-s195 <073ahome@gmail.com>
2025-10-04 23:30:50 +0530

    Add files via upload

---
+++ assignment 1 Q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b
+    printf("Enter the first number:");
+scanf("%d\n",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+sum = a+b;
+printf("The sum: %d",sum);
+return 0;
+
+}
