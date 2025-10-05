fbe4e31dda042caf2b79c4864b4af342a2fe4feb
Jiya-s195 <073ahome@gmail.com>
2025-10-05 15:27:50 +0530

    Add files via upload

---
+++ assignment1 Q6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int hours;
+    int minutes;
+    printf("Enter no. of hours:");
+    scanf("%d",&hours);
+    minutes=hours*60;
+    printf("%d hours is equal to %d minutes.\n",hours,minutes);
+    return 0;
+}
