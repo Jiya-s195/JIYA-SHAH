7d181ae40aa5e5737aed2e4f307acc8f95ef2a0a
Jiya-s195 <073ahome@gmail.com>
2025-10-05 15:29:14 +0530

    Add files via upload

---
+++ assignment1 Q9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int dollar;
+int rupees;
+printf("Enter no. of rupees:");
+scanf("%d",&rupees);
+dollar=rupees/48;
+printf("%d dollar is equal to %d rupees",dollar,rupees);
+return 0; 
+}
